0895ef30848526f364069d0bb8e53e26e4f1a249
oliverwk <30601311+oliverwk@users.noreply.github.com>
2020-11-04 10:36:50 +0100

    Delete rimg.py

--- rimg.py
+++
@@ -1,7 +0,0 @@
-import pyexiv2
-import pprint
-filename='art01.png'
-metadata = pyexiv2.ImageMetadata(filename)
-metadata.read()
-#userdata=json.loads(metadata['Exif.Photo.UserComment'].value)
-pprint.pprint(userdata)
